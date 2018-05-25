47da74bed4d293aa81774afeaea33a88014930e0
Mohamed Hassan <mohamed.h.khalifa@gmail.com>
2018-05-25 05:13:41 +0200

    Eclipse Project is created

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * This is our Main class, where the main method exists.  
+ */
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
